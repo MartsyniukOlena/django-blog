2323fab073889074097b2ca4a3f00ffbdcf64917
MartsyniukOlena <>
2024-02-15 15:31:33 +0000

    Add base template and homepage

--- blog/admin.py
+++ blog/admin.py
@@ -5,11 +5,12 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     list_filter = ('status',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+    list_filter = ('created_on',)
 
 # Register your models here.
 admin.site.register(Comment)
